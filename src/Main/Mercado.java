319b63398bea2a57b524446cb4d30a279b6c02a0
ArturHSousa <artuzeira100@gmail.com>
2024-04-24 14:10:22 -0300

    Iniciando switch case e primeiro metodo

--- src/Main/Mercado.java
+++ src/Main/Mercado.java
@@ -21,23 +21,59 @@ public class Mercado {
 
 
     private static void menu () {
-    try {
-        System.out.println("____________________________________________");
-        System.out.println("          Bem Vindos ao ArturMaket          ");
-        System.out.println("--------------------------------------------");
-        System.out.println("         Digite a opção que deseja          ");
-        System.out.println("____________________________________________");
-        System.out.println("|  Opção 1 - Cadastrar      |");
-        System.out.println("|  Opção 2 - Listar         |");
-        System.out.println("|  Opção 3 - Comprar        |");
-        System.out.println("|  Opção 4 - Carrinho       |");
-        System.out.println("|  Opção 5 - Sair           |");
-        int option = sc.nextInt();
+        int option = 0;
+        try {
+            System.out.println("____________________________________________");
+            System.out.println("          Bem Vindos ao ArturMaket          ");
+            System.out.println("--------------------------------------------");
+            System.out.println("         Digite a opção que deseja          ");
+            System.out.println("____________________________________________");
+            System.out.println("|  Opção 1 - Cadastrar      |");
+            System.out.println("|  Opção 2 - Listar         |");
+            System.out.println("|  Opção 3 - Comprar        |");
+            System.out.println("|  Opção 4 - Carrinho       |");
+            System.out.println("|  Opção 5 - Sair           |");
+            option = sc.nextInt();
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("Não Digite Letras, apenas números!");
         }
-    catch(InputMismatchException e){
-        System.out.println("Não Digite Letras, apenas números!");
+
+        switch (option) {
+            case 1:
+                cadastrarProdutos();
+                break;
+            case 2:
+                listarProdutos();
+                break;
+            case 3:
+                comprarProdutos();
+                break;
+            case 4:
+                verCarrrinho();
+                break;
+            case 5:
+                System.out.println("Obrigado por usar nosso sistema!");
+                System.exit();
+            default:
+                System.out.println("Opção inválida!");
         }
     }
+
+    private static void cadastrarProdutos(){
+        System.out.println("Nome do produto: ");
+        String nome = sc.next();
+
+        System.out.println("Preço do produto: ");
+        Double price = sc.nextDouble();
+
+        Produto produto = new Produto(nome, price);  //"Produto" = Produto do package "Model" ||  "produtos" = ArrayList || "produto" = instanciação do "Produto"
+        produtos.add(produto); //adicionou o produto ao ArrayList "produtos"
+        System.out.println(produto.getNome() + "foi cadastrado com sucesso!");
+        menu();
+    }
+
 
 
 }
